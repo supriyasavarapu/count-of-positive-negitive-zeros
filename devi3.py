8cb9447fcb56a678a385f6ac429be98c208b5c3b
supriyasavarapu <146428159+supriyasavarapu@users.noreply.github.com>
2025-07-08 20:46:13 +0700

    Add files via upload

---
+++ devi3.py
@@ -0,0 +1,21 @@
+# n=int(input())
+# Z=list(map(int,input().split())) 
+# pcount = 0
+# ncount = 0
+# zerocount = 0
+# for i in Z:
+#     if i>0:
+#         pcount += 1
+#     elif i<0:
+#         ncount += 1
+#     else:
+#         zerocount += 1
+
+# print(round((pcount/n),6))        
+# print(round((ncount/n),6))
+# print(round((zerocount/n),6))       #o/p=6
+#                                     #3 4 5 -8 0 -9
+#                                     #0.5
+#                                     #0.333333
+#                                     #0.166667
+ 
